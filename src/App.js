1461094f4f95aebcaf77ec0e77d391105ae76afc
sankar patra <sankarpatra573@gmail.com>
2024-02-05 15:11:26 +0530

    done

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import ExpenseItem from "./components/ExpenseItem";
 function App() {
   const expense = [
     { title: 'Car Insurance', amount: 295, date: new Date(2024, 2, 2) },
-    { title: 'Car Insurance', amount: 295, date: new Date(2024, 2, 2) },
-    { title: 'Car Insurance', amount: 295, date: new Date(2024, 2, 2) }
+    { title: 'Car Insurance', amount: 300, date: new Date(2024, 3, 3) },
+    { title: 'Car Insurance', amount: 395, date: new Date(2024, 4, 4) }
   ];
 
   return (
     <div className="App">
+      <h2>Let's get Started!</h2>
       {expense.map((expen, index) => (
         <ExpenseItem
           key={index}
